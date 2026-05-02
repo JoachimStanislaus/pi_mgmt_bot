af0a8f9ab0f6d032d710d547dfc6cb04ebebdd2a
Joachim Tan <joachim@Joachims-Mac-mini.local>
2026-05-02 21:21:30 +0100

    add temp and cpu and refactored ip

--- pi_mgmt_bot.py
+++ pi_mgmt_bot.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+
+import psutil
 from dotenv import load_dotenv
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 import json
@@ -29,6 +31,48 @@ def UserCheck(message):
         print(message.from_user.id)
         return False
 
+def cpu_usage():
+    try:
+        CPULOARD= psutil.cpu_percent()
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        CPULOARD = "Error retrieving CPU usage."
+    return CPULOARD
+
+def measure_temp():
+    try:
+        temp = os.popen("vcgencmd measure_temp").readline()                    
+        return (temp.replace("temp=","").replace("'C","").replace("\n",""))
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return "Error retrieving temperature."
+
+def get_ip_address():
+    try:
+        arg = 'hostname'  # Linux command to retrieve device's name
+        # Runs 'arg' in a hidden terminal.
+        p = subprocess.Popen(arg, shell=True, stdout=subprocess.PIPE)
+        data = p.communicate()  # Get data from 'p terminal'.
+        # Parse out device name
+        name_lines = data[0].splitlines()
+        name_line = name_lines[0].split()
+        name = name_line[0]
+        device = '%s' % (name.decode())
+
+        arg = 'hostname -I'  # Linux command to retrieve ip addresses
+        # Runs 'arg' in a hidden terminal.
+        p = subprocess.Popen(arg, shell=True, stdout=subprocess.PIPE)
+        data = p.communicate()  # Get data from 'p terminal'.
+        # Parse out ip address of device
+        ip_lines = data[0].splitlines()
+        split_line = ip_lines[0].split()
+        ipaddr = split_line[0]
+        my_ip = '%s' % (ipaddr.decode())
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        my_ip = "Error retrieving IP address."
+    return my_ip
+
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
@@ -38,36 +82,30 @@ def send_welcome(message):
     else:
         pass
 
-
-
-# Add this function with your other command handlers
+# Get IP address of device and send to user
 @bot.message_handler(commands=['ip'])
 def get_ip(message):
     if UserCheck(message) == True:
-        print("Getting IP address...")
-        try:
-            arg = 'hostname'  # Linux command to retrieve device's name
-            # Runs 'arg' in a hidden terminal.
-            p = subprocess.Popen(arg, shell=True, stdout=subprocess.PIPE)
-            data = p.communicate()  # Get data from 'p terminal'.
-            # Parse out device name
-            name_lines = data[0].splitlines()
-            name_line = name_lines[0].split()
-            name = name_line[0]
-            device = '%s' % (name.decode())
+        my_ip = get_ip_address()
+        bot.send_message(message.chat.id, my_ip)
+    else:
+        pass
 
-            arg = 'hostname -I'  # Linux command to retrieve ip addresses
-            # Runs 'arg' in a hidden terminal.
-            p = subprocess.Popen(arg, shell=True, stdout=subprocess.PIPE)
-            data = p.communicate()  # Get data from 'p terminal'.
-            # Parse out ip address of device
-            ip_lines = data[0].splitlines()
-            split_line = ip_lines[0].split()
-            ipaddr = split_line[0]
-            my_ip = '%s' % (ipaddr.decode())
-            bot.send_message(message.chat.id, my_ip)
-        except Exception:
-            bot.send_message(message.chat.id, "Error retrieving IP address.")
+# Get temperature of device and send to user
+@bot.message_handler(commands=['temp'])
+def get_temp(message):
+    if UserCheck(message) == True:
+        my_temp = measure_temp()
+        bot.send_message(message.chat.id, my_temp)
+    else:
+        pass
+
+# Get CPU usage of device and send to user
+@bot.message_handler(commands=['cpu'])
+def get_cpu(message):
+    if UserCheck(message) == True:
+        my_cpu = cpu_usage()
+        bot.send_message(message.chat.id, f"CPU Usage: {my_cpu}%")
     else:
         pass
 
